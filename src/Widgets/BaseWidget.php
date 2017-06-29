5bbcd2c6d8735c97803d4a7dc8adf1dbc5a1367b
Olajide Oye <olajide.oye@gmail.com>
2017-06-29 12:50:55 +0100

    [Widgets]: BaseWidget where we can safely carry out further customizations for our widgets

---
+++ src/Widgets/BaseWidget.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CottaCush\Yii2\Widgets;
+
+use yii\base\Widget;
+
+/**
+ * Class BaseWidget
+ * Base Class to carry further customizations across all CottaCush Yii2 widgets
+ * @author Olajide Oye <jide@cottacush.com>
+ * @package CottaCush\Yii2\Widgets
+ */
+class BaseWidget extends Widget
+{
+
+}
