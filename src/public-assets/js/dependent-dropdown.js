ecb1b72f740358fb6ccc1e87c2dbfda7911f5c46
KehindeLadipo <frequency20>
2017-09-23 22:40:38 +0100

    Cleanup: Reduced init parameters for the drop-down

--- src/public-assets/js/dependent-dropdown.js
+++ src/public-assets/js/dependent-dropdown.js
@@ -1,19 +1,21 @@
 var Dropdown = (function ($) {
-    return {
-        init: function(formEl, className, url) {
-            $(formEl).on('change', 'select' + className, function () {
-                var $element = $(this);
-                var $classEl = $(className);
-                $classEl.slice($classEl.index($element) + 1).remove();
-                $element.addClass('loading');
 
-                $.post(url, {'parent_id': $element.val()}, function (response) {
-                    $element.removeClass('loading');
+    return {
+        init: function (formEl) {
+            $(formEl).on('change', 'select.dep-drop',  function () {
+                var $element = $(this);
+                $element.parent().next('.subs').remove();
+                $element.addClass('dependent-dropdown-loading');
+
+                $.post($element.data('url'), {'parent_id': $element.val()}, function(response) {
+                    $element.removeClass('dependent-dropdown-loading');
+
+                    $element.parent().after('<div class="subs"></div>');
                     $element.parent().next('.subs').append(response.data);
                 });
             });
         }
-    };
 
+    };
 
 })(jQuery);
